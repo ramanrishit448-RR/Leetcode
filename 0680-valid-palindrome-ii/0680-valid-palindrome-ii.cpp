a0f2af27abd8cb75822a8036fb62c7d7d1d4e915
Rishit Raman <ramanrishit448@gmail.com>
2026-09-10 14:42:39 +0530

    Time: 3 ms (68.62%), Space: 22.1 MB (56.09%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] == s[right]) {
+                left++;
+                right--;
+            } else {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+
+        return true;
+    }
+};
