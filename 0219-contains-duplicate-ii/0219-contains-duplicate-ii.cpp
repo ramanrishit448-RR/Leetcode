cb3d0002851601718dc28285046b4dd4834b3d89
Rishit Raman <ramanrishit448@gmail.com>
2026-09-12 16:15:43 +0530

    Time: 129 ms (19.21%), Space: 134 MB (39.06%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,26 +1,24 @@
-#include <vector>
 #include <unordered_set>
+#include <vector>
 
 class Solution {
 public:
     bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
-        std::unordered_set<int> set;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            // Remove the element that is now outside the window
-            if (i > k) {
-                set.erase(nums[i - k - 1]);
-            }
-            
-            // Check if the current element exists in the set
-            if (set.count(nums[i])) {
+        unordered_set<int> window;
+
+        for (int i = 0; i < nums.size(); i++) {
+            // If the current number is already in the set, we found a match within distance k
+            if (window.count(nums[i])) {
                 return true;
             }
-            
-            // Add the current element to the set
-            set.insert(nums[i]);
+            // Add the current number to the set
+            window.insert(nums[i]);
+
+            // If the window size exceeds k, remove the oldest element
+            if (window.size() > k) {
+                window.erase(nums[i - k]);
+            }
         }
-        
         return false;
     }
 };
